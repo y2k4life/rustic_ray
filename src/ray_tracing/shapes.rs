edccabe5c13724bae14dd33bf8d9c5bf7a47242e
Guy <dotat@hotmail.com>
2020-11-15 19:53:08 -0800

    Chapter 13 Cubes

--- src/ray_tracing/shapes.rs
+++ src/ray_tracing/shapes.rs
@@ -2,8 +2,10 @@ pub mod sphere;
 pub mod test_shape;
 pub mod shape;
 pub mod plane;
+pub mod cube;
 
 pub use sphere::Sphere;
 pub use test_shape::TestShape;
 pub use shape::Shape;
 pub use plane::Plane;
+pub use cube::Cube;
--- src/ray_tracing/shapes/cube.rs
+++ src/ray_tracing/shapes/cube.rs
@@ -1,6 +1,6 @@
 use super::Shape;
-use crate::{ray_tracing::matrix::IDENTITY, Intersection, Material, Matrix, Point, Ray, Vector};
-use std::any::Any;
+use crate::{Intersection, Material, Matrix, Point, Ray, Vector, ray_tracing::matrix::IDENTITY, float_cmp};
+use std::{any::Any};
 
 #[derive(Debug)]
 pub struct Cube {
@@ -15,6 +15,21 @@ impl Cube {
             material: Material::new(),
         }
     }
+
+    fn check_axis(&self, origin: f64, direction: f64) -> (f64, f64) {
+        let tmin_numerator = -1.0 - origin;
+        let tmax_numerator = 1.0 - origin;
+        
+        let tmin = tmin_numerator / direction;
+        let tmax = tmax_numerator / direction;
+
+        if tmin > tmax {
+            (tmax, tmin)
+        }
+        else {
+            (tmin, tmax)
+        }
+    }
 }
 
 impl Shape for Cube {
@@ -26,10 +41,6 @@ impl Shape for Cube {
         other.downcast_ref::<Self>().map_or(false, |a| self == a)
     }
 
-    fn shape_clone(&self) -> Box<dyn Shape> {
-        Box::new((*self).clone())
-    }
-
     fn transform(&self) -> Matrix {
         self.transform
     }
@@ -39,7 +50,11 @@ impl Shape for Cube {
     }
 
     fn material(&self) -> &Material {
-        self.material
+        &self.material
+    }
+
+    fn material_mut(&mut self) -> &mut Material {
+        &mut self.material
     }
 
     fn set_material(&mut self, material: Material) {
@@ -47,19 +62,47 @@ impl Shape for Cube {
     }
 
     fn local_intersect(&self, ray: Ray) -> Option<Vec<Intersection>> {
-        Some(vec![
-            Intersection::new(ray.origin.x, self),
-            Intersection::new(ray.origin.y, self),
-            Intersection::new(ray.origin.z, self),
-            Intersection::new(ray.direction.x, self),
-            Intersection::new(ray.direction.y, self),
-            Intersection::new(ray.direction.z, self),
-            ])
-        
+        let (xtmin, xtmax) = self.check_axis(ray.origin.x, ray.direction.x);
+        let (ytmin, ytmax) = self.check_axis(ray.origin.y, ray.direction.y);
+        let (ztmin, ztmax) = self.check_axis(ray.origin.z, ray.direction.z);
+
+        let min_values = [xtmin, ytmin, ztmin];
+        let tmin = min_values
+            .iter()
+            .max_by(|x, y| float_cmp(**x, **y));
+        let max_values = [xtmax, ytmax, ztmax];
+        let tmax = 
+            max_values.iter()
+            .min_by(|x, y| float_cmp(**x, **y));
+
+        let tmin = *tmin.unwrap();
+        let tmax = *tmax.unwrap();
+
+        if tmin > tmax {
+            None
+        }
+        else {
+            Some(vec![Intersection::new(tmin, self), Intersection::new(tmax, self)])
+        }
     }
 
     fn local_normal_at(&self, point: Point) -> Vector {
-        Vector::new(point.x, point.y, point.z)
+        let max_values = [point.x.abs(), point.y.abs(), point.z.abs()];
+        let maxc = 
+            max_values.iter()
+            .max_by(|x, y| float_cmp(**x, **y));
+        
+        let maxc = *maxc.unwrap();
+
+        if maxc == point.x.abs() {
+            Vector::new(point.x, 0.0, 0.0)
+        }
+        else if maxc == point.y.abs() {
+            Vector::new(0.0, point.y, 0.0)
+        }
+        else {
+            Vector::new(0.0, 0.0, point.z)
+        }
     }
 }
 
@@ -71,5 +114,67 @@ impl PartialEq for Cube {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
+    // Chapter 12 Cubes
+    // Page 168
+    #[test]
+    fn a_ray_intersects_a_cube() {
+        let c = Cube::new();
+        let data = vec![
+            (Point::new(5.0, 0.5, 0.0), Vector::new(-1.0, 0.0, 0.0), 4.0, 6.0),
+            (Point::new(-5.0, 0.5, 0.0), Vector::new(1.0, 0.0, 0.0), 4.0, 6.0),
+            (Point::new(0.5, 5.0, 0.0), Vector::new(0.0, -1.0, 0.0), 4.0, 6.0),
+            (Point::new(0.5, -5.0, 0.0), Vector::new(0.0, 1.0, 0.0), 4.0, 6.0),
+            (Point::new(0.5, 0.0, 5.0), Vector::new(0.0, 0.0, -1.0), 4.0, 6.0),
+            (Point::new(0.5, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0), 4.0, 6.0),
+            (Point::new(0.0, 0.5, 0.0), Vector::new(0.0, 0.0, 1.0), -1.0, 1.0)];
+        for rec in data {
+            let r = Ray::new(rec.0, rec.1);
+            let xs = c.local_intersect(r).unwrap();
+            assert_eq!(2, xs.len());
+            assert_eq!(xs[0].t, rec.2);
+            assert_eq!(xs[1].t, rec.3);
+        }
+    }
+
+    // Chapter 12 Cubes
+    // Page 172
+    #[test]
+    fn a_ray_misses_a_cube() {
+        let c = Cube::new();
+        let data = vec![
+            (Point::new(-2.0, 0.0, 0.0), Vector::new(0.2673, 0.5345, 0.8018)),
+            (Point::new(0.0, -2.0, 0.0), Vector::new(0.8018, 0.2673, 0.5345)),
+            (Point::new(0.0, 0.0, -2.0), Vector::new(0.5345, 0.8018, 0.2673)),
+            (Point::new(2.0, 0.0, 2.0), Vector::new(0.0, 0.0, -1.0)),
+            (Point::new(0.0, 2.0, 2.0), Vector::new(0.0, -1.0, 0.0)),
+            (Point::new(2.0, 2.0, 0.0), Vector::new(-1.0, 0.0, 0.0))];
+        for rec in data {
+            let r = Ray::new(rec.0, rec.1);
+            let xs = c.local_intersect(r);
+            assert_eq!(None, xs);
+        }
+    }
+
+    // Chapter 12 Cubes
+    // Page 173 & 174
+    #[test]
+    fn the_normal_on_the_surface_of_a_cube() {
+        let c = Cube::new();
+        let data = vec![
+            (Point::new(1.0, 0.5, -0.8), Vector::new(1.0, 0.0, 0.0)),
+            (Point::new(-1.0, -0.2, 0.9), Vector::new(-1.0, 0.0, 0.0)),
+            (Point::new(-0.4, 1.0, -0.1), Vector::new(0.0, 1.0, 0.0)),
+            (Point::new(0.3, -1.0, -0.7), Vector::new(0.0, -1.0, 0.0)),
+            (Point::new(-0.6, 0.3, 1.0), Vector::new(0.0, 0.0, 1.0)),
+            (Point::new(0.4, 0.4, -1.0), Vector::new(0.0, 0.0, -1.0)),
+            (Point::new(1.0, 1.0, 1.0), Vector::new(1.0, 0.0, 0.0)),
+            (Point::new(-1.0, -1.0, -1.0), Vector::new(-1.0, 0.0, 0.0))];
+        for rec in data {
+            let p = rec.0;
+            let normal = c.local_normal_at(p);
+            assert_eq!(rec.1, normal);
+        }
+    }
 }
